f4738bd31caafbc69927f134d5754cb8351ca29a
Recasted <lijkvocht1@gmail.com>
2026-07-14 16:31:38 +0200

    Make card 03 the projects card

--- config.js
+++ config.js
@@ -36,7 +36,7 @@ window.ARCHIVE_CONFIG = {
       ]
     },
     {
-      title: "VICE", subtitle: "archive 003", image: "assets/card-03.png", x: 57, y: 55, rotation: -2, theme: "void", audio: "",
+      title: "PROJECTS", subtitle: "projects 003", image: "assets/card-03.png", x: 57, y: 55, rotation: -2, theme: "void", audio: "",
       audioTracks: [
         { title: "RUN", src: "assets/audio/03-1-run.mp3" },
         { title: "reminding", src: "assets/audio/03-2-reminding.mp3" }
